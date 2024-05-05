0e6c6e94c7a566de2c94ad961e4a378d318b6875
Leonard Raspi <leooo.colin@gmail.com>
2024-05-05 03:03:51 +0200

    Comment unexistant fonts to avoid errors

--- display.py
+++ display.py
@@ -9,19 +9,18 @@ if font_choice == 1:
     project_font = "font/Architects_Daughter/ArchitectsDaughter-Regular.ttf"
 elif font_choice == 2:
     project_font = "font/Inconsolata/static/Inconsolata-SemiBold.ttf"
-elif font_choice == 3:
-    project_font = "font/Comfortaa/static/Comfortaa-Light.ttf"
+# elif font_choice == 3:
+#     project_font = "font/Comfortaa/static/Comfortaa-Light.ttf"
 elif font_choice == 4:
     project_font = "font/Open_Sans/OpenSans-SemiBold.ttf"
-elif font_choice == 5:
-    project_font = "font/Roboto/Roboto-Regular.ttf"
-elif font_choice == 6:
-    project_font = "font/Roboto_Slab/static/RobotoSlab-Regular.ttf"
-elif font_choice == 7:
-    project_font = "font/Ubuntu_Mono/UbuntuMono-Bold.ttf"
+# elif font_choice == 5:
+#     project_font = "font/Roboto/Roboto-Regular.ttf"
+# elif font_choice == 6:
+#     project_font = "font/Roboto_Slab/static/RobotoSlab-Regular.ttf"
+# elif font_choice == 7:
+#     project_font = "font/Ubuntu_Mono/UbuntuMono-Bold.ttf"
 else:
     project_font = "font/Open_Sans/OpenSans-SemiBold.ttf"
-
 
 font8 = ImageFont.truetype(project_font, 8)
 font12 = ImageFont.truetype(project_font, 12)
