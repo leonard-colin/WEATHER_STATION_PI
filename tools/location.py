f5b812febe2594b706501e31a36b4404298aab2e
Leonard Raspi <leooo.colin@gmail.com>
2025-02-03 23:41:54 +0100

    Add weqther forecqst IP location tool

---
+++ tools/location.py
@@ -0,0 +1,17 @@
+import requests
+import os
+
+LAT = os.environ.get("LATITUDE")
+LON = os.environ.get("LONGITUDE")
+CITY = os.environ.get("CITY")
+
+def get_location():
+    try:
+        response = requests.get("http://ip-api.com/json/")
+        data = response.json()
+        if data["status"] == "success":
+            return data["lat"], data["lon"], data["city"]
+        else:
+            return LAT, LON, CITY
+    except Exception:
+        return LAT, LON, CITY
